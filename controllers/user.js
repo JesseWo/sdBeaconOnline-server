5d902f5ae0a310397f2d3c224633cef34fff38f4
王振兴 <wangzhenxingvip@gmail.com>
2018-05-03 18:41:51 +0800

    FIX:接口参数

--- controllers/user.js
+++ controllers/user.js
@@ -1,18 +1,18 @@
 'use strict'
 
 /**
- * 查询用户状态
+ * 查询题库Id
  * @param {*} ctx 
  * @param {*} next 
  */
 var getChapterid = async (ctx, next) => {
-    console.log(`getChapterid userId: ${ctx.params.userId}`);
-
+    console.log(`getChapterid userId: ${ctx.query.userId}`);
+    
     let result = require('../data/qb_chapterid.json')
     ctx.response.body = result;
 };
 
 module.exports = {
-    'GET /sdbeacononline/getchapterid/:userId': getChapterid,
+    'GET /sdbeacononline/getchapterid': getChapterid,
 
 }
